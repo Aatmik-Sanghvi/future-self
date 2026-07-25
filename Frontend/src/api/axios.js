579c7e2cb5b9165751a6c9f36effda6ea3eff549
Aatmik <aatmik.zestbrains@gmail.com>
2026-07-25 21:42:15 +0530

    updated the axios.js, vite.config.js, yml file as well and added .env files for the prod. and github workflow

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: '/api/V1/',
+    baseURL: import.meta.VITE_API_URL,
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -17,20 +17,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server: {
-    proxy: {
-      // Proxy all requests starting with `/api` to your Laravel backend
-      '/api': {
-        target: 'http://127.0.0.1:8000', // Your Laravel backend URL
-        changeOrigin: true, // Needed for virtual hosted sites
-        secure: false, // If you're using HTTPS, set this to true
-        // Remove the rewrite function to preserve the `/api` prefix
-      },
-      '/storage': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 })
